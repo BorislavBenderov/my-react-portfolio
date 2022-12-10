78caf7d1473842db923645a1e0f46b3244918ee5
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-10 09:40:13 +0200

    Make changes in experience component and experience component css

--- src/components/experience/Experiance.js
+++ src/components/experience/Experiance.js
@@ -6,48 +6,12 @@ export const Experience = () => {
         <section id="experience">
             <h5>What Skills I Have</h5>
             <h1>My Experience</h1>
-
             <div className='container experience__container'>
                 <div className="experience__frontend">
-                    <h3>Frontend Development</h3>
-                    <div className="experience__content">
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>JavaScript</h4>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>HTML</h4>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>CSS</h4>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>React</h4>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>REST API</h4>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon' />
-                            <div>
-                                <h4>AJAX</h4>
-                            </div>
-                        </article>
-                    </div>
+                    <h3>I can build single page and multi page applications with React JS, AJAX, REST Services, CRUD Operations,
+                        React Router, Context API, Authentication, Firebase, HTML, CSS, Node JS, Express, Handlebars, MongoDB, Mongoose,
+                        Cookies, Sessions, JSON Web Token.
+                    </h3>
                 </div>
             </div>
         </section>
