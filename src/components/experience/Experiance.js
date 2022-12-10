c487d0367ae2b988f95de963ba3a7c16d57e410d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-10 11:18:19 +0200

    Update experience component title

--- src/components/experience/Experiance.js
+++ src/components/experience/Experiance.js
@@ -1,5 +1,4 @@
 import './experience.css';
-import { BsPatchCheckFill } from 'react-icons/bs';
 
 export const Experience = () => {
     return (
@@ -8,9 +7,9 @@ export const Experience = () => {
             <h1>My Experience</h1>
             <div className='container experience__container'>
                 <div className="experience__frontend">
-                    <h3>I can build single page and multi page applications with React JS, AJAX, REST Services, CRUD Operations,
-                        React Router, Context API, Authentication, Firebase, HTML, CSS, Node JS, Express, Handlebars, MongoDB, Mongoose,
-                        Cookies, Sessions, JSON Web Token.
+                    <h3>I can build single page and multi page applications with React JS, HTML, CSS, AJAX, REST Services, CRUD Operations,
+                        React Router, Context API, Authentication, Firebase, Node JS, Express, Handlebars, MongoDB, Mongoose,
+                        Cookies, Sessions, JSON Web Token, etc.
                     </h3>
                 </div>
             </div>
